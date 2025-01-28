848ca8475be84fa42fcb62b43610ba6ed7368e6c
Umut Akdeniz <akdenizumut375@gmail.com>
2025-01-28 13:10:53 +0300

    SatisRaporu.java

--- rapor/SatisRaporu.java
+++ rapor/SatisRaporu.java
@@ -1,21 +1,3 @@
-//package rapor;
-//
-//public class SatisRaporu extends Raporlama {
-//    public SatisRaporu() {
-//        super(getKategori(), urunAdi, fiyat, adet);
-//    }
-//
-//    public Object[][] getSatisRaporuVerisi() {
-//        // Örnek veriler döndürüyoruz, burada veritabanından veya diğer kaynaklardan veri alabilirsiniz
-//        return new Object[][] {
-//                {"Elektronik", "Telefon", 1500.0, 10, 15000.0},
-//                {"Elektronik", "Laptop", 3500.0, 5, 17500.0},
-//                {"Giyim", "T-Shirt", 50.0, 30, 1500.0}
-//        };
-//    }
-//
-//}
-
 package rapor;
 
 public class SatisRaporu extends Raporlama {
